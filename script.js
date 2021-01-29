4e49736a59fec5c06a2fac3017121e508f6d9d07
Ataur Rahman <rahman.sharif39n@gmail.com>
2021-01-29 18:34:55 +0600

    plus minus done but minus number stil counting

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// function for phone item------
+
+//system -1 (general type code)
+
 // document.getElementById("phnQntyPlus").addEventListener("click",function(){
 //     const phnInput= document.getElementById("phnQnty");
 //     const phnCount = parseInt(phnInput.value);
@@ -18,13 +22,18 @@
 // })
 
 
-document.getElementById("phnQntyPlus").addEventListener("click", function () {
-    ProductChange(true);
-})
+//system -2  (code with simple function)
 
-document.getElementById("phnQntyMinus").addEventListener("click", function () {
-    ProductChange(false);
-})
+// document.getElementById("phnQntyPlus").addEventListener("click", function () {
+//     ProductChange(true);
+// })
+
+// document.getElementById("phnQntyMinus").addEventListener("click", function () {
+//     ProductChange(false);
+// })
+
+//system -3 (code with full function)
+
 
 function ProductChange(increase) {
 
@@ -37,8 +46,57 @@ function ProductChange(increase) {
     if (increase == false && phnCount > 0) {
         phnNewCount = phnCount - 1;
     }
+
     phnInput.value = phnNewCount;
     const phnTotalCost = phnNewCount * 1219;
     document.getElementById("phnTotal").innerText = "$" + phnTotalCost;
 
+}
+
+// function for case Item ------
+
+// system -1 (general type code)
+
+// document.getElementById("caseQntyPlus").addEventListener("click", function () {
+//     const caseInput = document.getElementById("caseQnty");
+//     const caseInputValue = parseInt(caseInput.value);
+//     const caseNewInputValue = caseInputValue + 1;
+//     caseInput.value = caseNewInputValue;
+//     const caseTotal = caseNewInputValue * 59;
+//     document.getElementById("caseTotal").innerText = "$" + caseTotal;
+// })
+
+// document.getElementById("caseQntyMinus").addEventListener("click", function () {
+//     const caseInput = document.getElementById("caseQnty");
+//     const caseInputValue = parseInt(caseInput.value);
+//     caseNewInputValue = caseInputValue - 1;
+//     caseInput.value = caseNewInputValue;
+//     const caseTotal = caseNewInputValue * 59;
+//     document.getElementById("caseTotal").innerText = "$" + caseTotal;
+// })
+
+
+//system -2 (code with simple function)
+
+document.getElementById("caseQntyPlus").addEventListener("click", function () {
+    caseItem(true);
+})
+
+document.getElementById("caseQntyMinus").addEventListener("click", function () {
+    caseItem(false);
+})
+
+function caseItem (increase) {
+    const caseInput = document.getElementById("caseQnty");
+    const caseCount = parseInt(caseInput.value);
+    let caseNewCount = caseQnty;
+    if (increase == true){
+        caseNewCount = caseCount + 1;
+    }
+    if (increase == false){
+        caseNewCount= caseCount - 1;
+    }
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 59;
+    document.getElementById("caseTotal").innerText = "$" +caseTotal ;
 }
