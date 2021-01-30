a6479faa25212efea82a69c35c47a35ab4c7f0b0
Ataur Rahman <rahman.sharif39n@gmail.com>
2021-01-30 16:28:30 +0600

    code with shared function

--- script.js
+++ script.js
@@ -35,23 +35,23 @@
 //system -3 (phn item code with full function)
 
 
-function ProductChange(increase) {
+// function ProductChange(increase) {
 
-    const phnInput = document.getElementById("phnQnty");
-    const phnCount = parseInt(phnInput.value);
-    let phnNewCount = phnCount;
-    if (increase == true) {
-        phnNewCount = phnCount + 1;
-    }
-    if (increase == false && phnCount > 0) {
-        phnNewCount = phnCount - 1;
-    }
+//     const phnInput = document.getElementById("phnQnty");
+//     const phnCount = parseInt(phnInput.value);
+//     let phnNewCount = phnCount;
+//     if (increase == true) {
+//         phnNewCount = phnCount + 1;
+//     }
+//     if (increase == false && phnCount > 0) {
+//         phnNewCount = phnCount - 1;
+//     }
 
-    phnInput.value = phnNewCount;
-    const phnTotalCost = phnNewCount * 1219;
-    document.getElementById("phnTotal").innerText = "$" + phnTotalCost;
+//     phnInput.value = phnNewCount;
+//     const phnTotalCost = phnNewCount * 1219;
+//     document.getElementById("phnTotal").innerText = "$" + phnTotalCost;
 
-}
+// }
 
 // function for case Item ------
 
@@ -78,25 +78,74 @@ function ProductChange(increase) {
 
 //system -2 ( case item code with simple function)
 
-document.getElementById("caseQntyPlus").addEventListener("click", function () {
-    caseItem(true);
-})
+// document.getElementById("caseQntyPlus").addEventListener("click", function () {
+//     caseItem(true);
+// })
 
-document.getElementById("caseQntyMinus").addEventListener("click", function () {
-    caseItem(false);
-})
+// document.getElementById("caseQntyMinus").addEventListener("click", function () {
+//     caseItem(false);
+// })
 
-function caseItem(increase) {
-    const caseInput = document.getElementById("caseQnty");
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
+
+//system-3 ( case item code full function)
+
+// function caseItem(product, increase) {
+//     const caseInput = document.getElementById(product);
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+//     if (increase == true) {
+//         caseNewCount = caseCount + 1;
+//     }
+//     if (increase == false && caseCount > 0) {
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById("caseTotal").innerText = "$" + caseTotal;
+// }
+
+
+
+//shared function
+
+function ProductChange(product,increase) {
+
+    const productInput = document.getElementById(product);
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
     if (increase == true) {
-        caseNewCount = caseCount + 1;
+        productNewCount = productCount + 1;
     }
-    if (increase == false && caseCount > 0) {
-        caseNewCount = caseCount - 1;
+    if (increase == false && productCount > 0) {
+        productNewCount = productCount - 1;
     }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("caseTotal").innerText = "$" + caseTotal;
+
+    productInput.value = productNewCount;
+    let productTotalCost = 0;
+    if (product == "phnQnty"){
+        productTotalCost = productNewCount * 1219 ;
+        document.getElementById("phnTotal").innerText = "$" + productTotalCost;
+    }
+    if ( product == "caseQnty"){
+        productTotalCost = productNewCount* 59 ;
+        document.getElementById("caseTotal").innerText = "$" + productTotalCost;
+    }
+    
+    subtotal();
+}
+function subtotal() {
+    const phnQnty = document.getElementById("phnQnty");
+    const phnQntyValue = parseInt(phnQnty.value);
+
+    const caseQnty = document.getElementById("caseQnty");
+    const caseQntyValue = parseInt(caseQnty.value);
+
+    const subtotal = phnQntyValue * 1219 + caseQntyValue * 59 ;
+    document.getElementById("total").innerText = "$" + subtotal;
+
+    const tax = subtotal *0.15;
+    document.getElementById("tax").innerText = "$" + tax.toFixed(2);
+
+    const grandTotal = subtotal + tax;
+    document.getElementById("grandTotal").innerText = "$" + grandTotal;
 }
